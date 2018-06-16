8150ea94804d9e6ce1fdc6fe23a65575022e4f5a
Ryan Stillings <rystills@gmail.com>
2018-06-16 13:24:25 -0700

    Flesh out index and login pages with bootstrap, add minimum password length and existing username checks

--- backend/createAccount.php
+++ backend/createAccount.php
@@ -5,15 +5,21 @@ $username = "rising64_admin";
 $password = "admin";
 $dbname = "rising64_data";
 
+$name = $_POST['username'];
+$pass = $_POST['password'];
+
+//verify that credentials meet requirements
+if (strlen($pass) < 6) {
+	die("Error: password must be at least 6 characters long");
+	
+}
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-$name = $_POST['username'];
-$pass = $_POST['password'];
 
 $sql = "INSERT INTO users( username, 
 PASSWORD ) 
@@ -24,7 +30,7 @@ VALUES (
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: username already taken";
 }
 
 $conn->close();
