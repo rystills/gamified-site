ca055d86b4b2f50b0bc41db2f78b0b3c8cadb3b9
Ryan Stillings <rystills@gmail.com>
2018-06-16 23:21:27 -0700

    copy in boilerplate code for first supported game 'targetTest'

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # server-test  
 Experimenting with user accounts and database access through x10hosting.  
 Site theme by Bootstrap  
+  
+todo:  
+-salt + hash passwords  
+-move backend files outside of document root to prevent unauthorized access  
---
+++ games/main.js
@@ -0,0 +1,143 @@
+/**
+ * clear each canvas to a predetermined fillcolor, preparing it for a fresh render
+ */
+function clearScreen() {
+	//main canvas
+	ctx.fillStyle="#add8e6";
+	ctx.fillRect(0,0,cnv.width,cnv.height);
+	
+	//HUD canvas
+	uictx.fillStyle="rgb(0,0,0)";
+	uictx.fillRect(0,0,uicnv.width,uicnv.height);
+}
+
+/**
+ * render all objects and HUD elements
+ */
+function render() {
+	//clear all canvases for a fresh render
+	clearScreen();
+	
+	//draw all objects with images specified, centered in order of list indices
+	for (let i = 0; i < objects.length; ++i) {
+		if (objects[i].imgName) {
+			drawCentered(objects[i].imgName,ctx, objects[i].x, objects[i].y,objects[i].dir);	
+		}
+	}
+	
+	//finally draw the HUD
+	drawHUD();
+}
+
+/**
+ * draw the HUD
+ */
+function drawHUD() {
+	//draw buttons
+	for (var i = 0; i < buttons.length; ++i) {
+		var btnctx = buttons[i].canvas.getContext("2d");
+		//fill light blue border color
+		btnctx.fillStyle = "rgb(" +  
+		Math.round(.15 * buttons[i].blendWhiteness) + ", " + 
+		Math.round(buttons[i].blendWhiteness *.75) + ", " + 
+		Math.round(.1 * buttons[i].blendWhiteness) + ")";
+		btnctx.fillRect(buttons[i].x, buttons[i].y, buttons[i].width,buttons[i].height);
+		
+		//fill blue inner color
+		btnctx.fillStyle = "rgb(" + 
+		Math.round(buttons[i].blendWhiteness *.1) + ", " + 
+		Math.round(.15 * buttons[i].blendWhiteness) + ", " + 
+		Math.round(.75 * buttons[i].blendWhiteness) + ")";
+		btnctx.fillRect(buttons[i].x + 2, buttons[i].y + 2, buttons[i].width - 4,buttons[i].height - 4);
+		
+		//set the font size and color depending on the button's attributes and state
+		btnctx.font = buttons[i].fontSize + "px Arial";
+		btnctx.fillStyle = "rgb(" + buttons[i].blendWhiteness + ", " + buttons[i].blendWhiteness + ", " + buttons[i].blendWhiteness + ")";
+		
+		//draw the button label (add slight position offset to account for line spacing)
+		btnctx.fillText(buttons[i].text,buttons[i].x + 4, buttons[i].y + buttons[i].height/2 + 8);
+	}
+	uictx.font = "24px Arial";
+	uictx.fillStyle = "#FFFFFF";
+}
+
+/**
+ * main game loop; update all aspects of the game in-order
+ */
+function update() {
+	//update the deltaTime
+	updateTime();
+		
+	//update objects
+	for (let i = 0; i < objects.length; objects[i].update(), ++i);
+	
+	//update GUI elements
+	for (let i = 0; i < buttons.length; buttons[i].update(), ++i);
+	
+	//once all updates are out of the way, render the frame
+	render();
+	
+	//toggle off any one-frame event indicators at the end of the update tick
+	mousePressedLeft = false;
+	mousePressedRight = false;
+}
+
+/**
+ * initialize a reference to each of our canvases and contexts
+ */
+function initCanvases() {
+	//create appropriately named references to all of our canvases
+	cnv = document.getElementById("cnv");
+	ctx = cnv.getContext("2d");
+	
+	uicnv = document.getElementById("uicnv");
+	uictx = uicnv.getContext("2d");
+}
+
+/**
+ * load the asset loader, which will load all of our required elements in order
+ */
+function loadAssets() {
+	//setup a global, ordered list of asset files to load
+	requiredFiles = [
+		"src\\util.js","src\\setupKeyListeners.js", //misc functions
+		"src\\classes\\Enum.js", "src\\classes\\Button.js" //classes
+		];
+	
+	//manually load the asset loader
+	let script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = "src\\loadAssets.js";
+	script.onload = loadAssets;
+	//begin loading the asset loader by appending it to the document head
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+/**
+ * initialize all global variables
+ */
+function initGlobals() {
+	//keep a global fps flag for game-speed (although all speeds should use deltaTime)
+	fps = 60;
+	
+	//init global time vars for delta time calculation
+	prevTime = Date.now();
+	deltaTime = 0;
+	totalTime = 0;
+	
+	
+	//global game objects
+	objects = [];
+	
+	//global list of UI buttons
+	buttons = [];
+}
+
+//disallow right-click context menu as right click functionality is often necessary for gameplay
+document.body.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+//initialize a reference to the canvas first, then begin loading the game
+initCanvases();
+loadAssets();
