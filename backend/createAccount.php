06f374f87d83a7d07a4a1cb536dc207c1c175680
Ryan Stillings <rystills@gmail.com>
2018-06-27 23:59:39 -0700

    add password hashing, successful account creation automatically loads login page

--- backend/createAccount.php
+++ backend/createAccount.php
@@ -6,17 +6,18 @@ $pass = $_POST['password'];
 //verify that credentials meet requirements
 if (strlen($pass) < 6) {
 	die("Error: password must be at least 6 characters long");
-	
 }
+
+$hashed_password = password_hash($pass, PASSWORD_DEFAULT);
 
 $sql = "INSERT INTO users( username, 
 PASSWORD ) 
 VALUES (
-'".$name."', '".$pass."'
+'".$name."', '".$hashed_password."'
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+	echo "Success";
 } else {
     echo "Error: username already taken";
 }
--- backend/login.php
+++ backend/login.php
@@ -8,23 +8,24 @@
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
       $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
       
-      $sql = "SELECT id FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $sql = "SELECT password FROM users WHERE username = '$myusername'";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
       $count = mysqli_num_rows($result);
       mysqli_close($con);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
       if($count == 1) {
-         //add username to session data
-         $_SESSION['username'] = $myusername;
-         echo "Success";
-         exit;
+          $hashed_password = $row['password'];
+          if(password_verify($mypassword, $hashed_password)) {
+            //add username to session data
+            $_SESSION['username'] = $myusername;
+            echo "Success";
+            exit;
+          }
+          else {
+              echo "Error: invalid password";
+          }
       }else {
-         echo "Error: Invalid Username or Password";
+         echo "Error: User does not exist";
       }
    }
 ?>
