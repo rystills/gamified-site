44742ad3e0d05a2d206923384365135c883d2258
Ryan Stillings <rystills@gmail.com>
2018-06-29 01:44:14 -0700

    Manually position instructions from center align to ensure proper scaling

--- games/targetTest.php
+++ games/targetTest.php
@@ -19,13 +19,15 @@ include($_SERVER["DOCUMENT_ROOT"] . '/backend/session.php');
 		<canvas id="cnv" width="800" height="600"></canvas><canvas 
 		id="uicnv" width=320" height="600"></canvas>
 	</div>
-	<div class="unselectable">
-		<font color="white"><h2 style="padding-left: 400px; line-height: 0px;">Instructions:</h2>
+	<div class="unselectable" style="text-align:center">
+		<font color="white"><h2 style="margin-left:-958px; line-height: 0px;">Instructions:</h2>
 		<p style="font-size:0px"><br><bp>ont>
-		<h4 style="padding-left: 400px;"> 
-		&nbsp> Left click to interact with buttons and shoot<br>
-		&nbsp> Right click to stop currently active tool<br>
-		&nbsp> A/D to move left/right respectively</h4></font>
+		<h4 style="margin-left:-749px;line-height: 10px"> 
+		> Left click to interact with buttons and shoot<br></h4>
+		<h4 style="margin-left:-787px;line-height: 10px"> 
+		> Right click to stop currently active tool<br></h4>
+		<h4 style="margin-left:-820px;line-height: 10px">
+		> A/D to move left/right respectively</h4></font>
 	</div>
 	<script src="targetTest_main.js"></script>
 </body>
