52407fd1422863a0953d881a5bf9e36eac8d7d1b
Ryan Stillings <rystills@gmail.com>
2018-07-25 23:32:21 -0700

    add star particles to goal

--- games/ninjaNightmare_src/Goal.js
+++ games/ninjaNightmare_src/Goal.js
@@ -5,4 +5,17 @@
 makeChild("Goal","GameObject");
 function Goal(x,y,ctx) {
     GameObject.call(this,x,y,"goal",0,-1000,ctx);
+    this.particleMaxTimer = 5;
+    this.particleTimer = this.particleMaxTimer;
+}
+
+/**
+ * update the goal
+ */
+Goal.prototype.update = function() {
+    if (--this.particleTimer == 0) {
+        this.particleTimer = this.particleMaxTimer;
+        activeRoom.addParticle(new Particle(getRandomInt(this.x,this.x+images[this.imgName].width),getRandomInt(this.y,this.y+images[this.imgName].height),
+        40,"255,255,0",5,0,0,true,particleShapes.star));
+    }
 }
--- games/ninjaNightmare_src/Particle.js
+++ games/ninjaNightmare_src/Particle.js
@@ -1,3 +1,7 @@
+particleShapes = new Enum();
+particleShapes.add("circle");
+particleShapes.add("star");
+
 /**
  * Particle class; defines a basic particle for rendering effects
  * @param x: start x coord
@@ -10,7 +14,7 @@
  * @param fadeOut: whether or not to disappear gradually over time
  */
 makeChild("Particle","GameObject");
-function Particle(x,y,life,color,radius,xChange, yChange,fadeOut) {
+function Particle(x,y,life,color,radius,xChange, yChange,fadeOut,shape=particleShapes.circle) {
     GameObject.call(this,x,y,null,1000,1000);
     this.maxLife = life;
     this.life = life;
@@ -20,6 +24,7 @@ function Particle(x,y,life,color,radius,xChange, yChange,fadeOut) {
     this.color = color;
     this.fadeOut = fadeOut;
     this.opacity = 1;
+    this.shape = shape;
 }
 
 /**
@@ -39,7 +44,50 @@ Particle.prototype.update = function() {
  */
 Particle.prototype.render = function() {
     ctx.fillStyle = "rgba(" + this.color + ", " + this.opacity + ")";
+    ctx.strokeStyle = ctx.fillStyle;
+    switch(this.shape) {
+        case particleShapes.circle:
+            this.drawCircle();
+            break;
+        case particleShapes.star:
+            this.drawStar(5,this.radius,this.radius/2);
+            break;
+    } 
+}
+
+/**
+ * draw a circle
+ */
+Particle.prototype.drawCircle = function() {
     ctx.beginPath();
     ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
     ctx.fill();
+}
+
+/**
+ * draw a star - method adopted from https://stackoverflow.com/a/25840319
+ */
+Particle.prototype.drawStar = function(spikes,outerRadius,innerRadius) {
+      var rot=Math.PI/2*3;
+      var step=Math.PI/spikes;
+      var x = this.x;
+      var y = this.y;
+      ctx.beginPath();
+      ctx.moveTo(this.x,this.y-outerRadius)
+      for(i=0;i<spikes;i++){
+        x=this.x+Math.cos(rot)*outerRadius;
+        y=this.y+Math.sin(rot)*outerRadius;
+        ctx.lineTo(x,y)
+        rot+=step
+
+        x=this.x+Math.cos(rot)*innerRadius;
+        y=this.y+Math.sin(rot)*innerRadius;
+        ctx.lineTo(x,y)
+        rot+=step
+      }
+      ctx.lineTo(this.x,this.y-outerRadius);
+      ctx.closePath();
+      ctx.lineWidth=5;
+      ctx.stroke();
+      ctx.fill();
 }
